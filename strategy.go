903c097cea6288a67a039cc46e89797cb73dd0ea
qorcodusl <qorcodus03@naver.com>
2025-11-20 01:03:08 +0900

    Feat: 자동차와 이동전략 로직 추가
    자동차 전진 조건을 독립 시켜기

---
+++ strategy.go
@@ -0,0 +1,28 @@
+package goracing
+
+import "math/rand"
+
+// 이동 전략
+type MovementStrategy interface {
+	Movable() bool
+}
+
+// 랜덤 전략
+type RandomMovementStrategy struct{}
+
+func (r RandomMovementStrategy) Movable() bool {
+	return rand.Intn(10) >= 4
+}
+
+// 테스트용 전략
+type AlwaysMoveStrategy struct{}
+
+func (a AlwaysMoveStrategy) Movable() bool {
+	return true
+}
+
+type NeverMoveStrategy struct{}
+
+func (n NeverMoveStrategy) Movable() bool {
+	return false
+}
