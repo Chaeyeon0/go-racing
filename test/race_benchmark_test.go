ac90987544c55eb4398e4e92db44a00b69ef67d3
qorcodusl <qorcodus03@naver.com>
2025-11-23 22:23:10 +0900

    Test: 자동차 수 증가 시 얼마나 효율적으로 실행되는지 측정하기 위한 테스트

---
+++ test/race_benchmark_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"goracing/domain"
+	"goracing/tui"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func BenchmarkRacePerformance(b *testing.B) {
+	testCases := []int{10, 100, 1000}
+
+	for _, carCount := range testCases {
+		b.Run(
+			// 서브테스트 이름
+			string(rune(carCount)),
+			func(b *testing.B) {
+				names := make([]string, carCount)
+				for i := range names {
+					names[i] = "car" + string(rune('A'+i%26))
+				}
+
+				cars, _ := domain.NewCars(names)
+				race := tui.NewRace(cars.List)
+
+				start := time.Now()
+				updateCh, resultCh := race.Start()
+
+				// 실시간 업데이트 소비
+				go func() {
+					for range updateCh {
+					}
+				}()
+
+				<-resultCh
+				elapsed := time.Since(start)
+
+				b.ReportMetric(float64(runtime.NumGoroutine()), "goroutines")
+				b.ReportMetric(float64(elapsed.Milliseconds()), "ms")
+			})
+	}
+}
